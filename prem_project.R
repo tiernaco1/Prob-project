fd575ed57c6a97ab5b6818e96c42c73ec318668a
obriek40 <obriek40@tcd.ie>
2024-11-27 17:59:58 +0000

    Update prem_project.R
    simulates the rest of the season

--- prem_project.R
+++ prem_project.R
@@ -1,44 +1,88 @@
 library(dplyr)
-remaining_fixtures <- read.csv("remaining fixtures.csv")
-team_stats <- read.csv("Stats_for_PL.csv")
+remaining_fixtures <- read.csv("remaining fixtures.csv", header = TRUE)
+team_stats <- read.csv("Stats_for_PL.csv", header = TRUE)
 str(team_stats$Teams)
 
 simulate_game <- function(home_team, away_team, team_stats){
-
-home_stats <- team_stats %>% filter(Teams == home_team)
-away_stats <- team_stats %>% filter(Teams == away_team)
-
-home_xG_90 <- as.numeric(home_stats$xG.p90)
-home_xGA_90 <- as.numeric(home_stats$xGA..p90)
-away_xG_90 <- as.numeric(away_stats$xG.p90)
-away_xGA_90 <- as.numeric(away_stats$xGA..p90)
-
-home_xG <- home_xG_90 * 1.1
-away_xG <- away_xG_90 * 0.9
-
-home_xG_real <- (home_xG + away_xGA_90) / 2
-away_xG_real <- (away_xG + home_xGA_90) / 2
-
-home_goals <- rpois(1, lambda = home_xG_real)
-away_goals <- rpois(1, lambda = away_xG_real)
-
-if (home_goals > away_goals) {
-  home_result <- "Win"
-} else if (home_goals < away_goals) {
-  home_result <- "Loss"
-} else {
-  home_result <- "Draw"
-}
-
-return(list(
-  HomeTeam = home_team,
-  AwayTeam = away_team,
-  HomeResult = home_result,
-  HomeGoals = home_goals,
-  AwayGoals = away_goals,
-  ExpectedGoals = list(Home = home_xG_real, Away = away_xG_real)
-))
+  
+  home_stats <- team_stats %>% filter(Teams == home_team)
+  away_stats <- team_stats %>% filter(Teams == away_team)
+  
+  home_xG_90 <- as.numeric(home_stats$xG.p90)
+  home_xGA_90 <- as.numeric(home_stats$xGA..p90)
+  away_xG_90 <- as.numeric(away_stats$xG.p90)
+  away_xGA_90 <- as.numeric(away_stats$xGA..p90)
+  
+  home_xG <- home_xG_90 * 1.1
+  away_xG <- away_xG_90 * 0.9
+  
+  home_xG_real <- (home_xG + away_xGA_90) / 2
+  away_xG_real <- (away_xG + home_xGA_90) / 2
+  
+  home_goals <- rpois(1, lambda = home_xG_real)
+  away_goals <- rpois(1, lambda = away_xG_real)
+  
+  if (home_goals > away_goals) {
+    home_result <- "Win"
+  } else if (home_goals < away_goals) {
+    home_result <- "Loss"
+  } else {
+    home_result <- "Draw"
+  }
+  
+  return(list(
+    HomeTeam = home_team,
+    AwayTeam = away_team,
+    HomeResult = home_result,
+    HomeGoals = home_goals,
+    AwayGoals = away_goals,
+    ExpectedGoals = list(Home = home_xG_real, Away = away_xG_real)
+  ))
 
 }
 
 simulate_game("Man Utd", "Ipswich", team_stats)
+
+simulate_reamining_season <- function(remaining_fixtures, team_stats){
+  teams <- matrix(nrow = 20, ncol = 2) #create table 
+  for (i in 1:nrow(team_stats)) {
+    teams[i,1] <- team_stats$Teams[i]  #populate teams
+    teams[i,2] <- team_stats$Pts[i]    # populate thier current points
+  }
+  #print(teams)
+  #loops through each remaining fixture
+  for (i in 1:nrow(remaining_fixtures)) {
+    # Extract Home Team and Away Team for each row
+    home_team <- remaining_fixtures$Home.Team[i]
+    away_team <- remaining_fixtures$Away.Team[i]
+    
+    # Print or store the result (for demonstration purposes, we'll print)
+    #print(paste("Row", i, "- Home Team:", home_team, "Away Team:", away_team))
+    result <- simulate_game(home_team, away_team, team_stats)  # simulated game
+    #print(result$HomeResult)
+    
+    home_stats <- team_stats %>% filter(Teams == home_team)
+    home_startPos <- as.numeric(home_stats$position)          # find original postion in the table
+    away_stats <- team_stats %>% filter(Teams == away_team)
+    away_startPos <- as.numeric(home_stats$position)
+    # following section adds points to the respective teams 
+    if (result$HomeResult == "Win"){
+      points <- as.numeric(teams[home_startPos,2])
+      teams[home_startPos,2] <- points + 3
+    }
+    else if (result$HomeResult == "Loss"){
+      points <- as.numeric(teams[away_startPos,2])
+      teams[away_startPos,2] <- points + 3
+    }
+    else{
+      points <- as.numeric(teams[home_startPos,2])
+      teams[home_startPos,2] <- points + 1
+      points <- as.numeric(teams[away_startPos,2])
+      teams[away_startPos,2] <- points + 1
+    }
+  }
+  print(teams)
+}
+
+simulate_reamining_season(remaining_fixtures, team_stats)
+
